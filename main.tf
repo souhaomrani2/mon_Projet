ddb2adf2d272798b9564b2467033565b97c61a86
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-21 14:14:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-  pm_api_url      = https://192.168.127.134:8006/#v1:0:=qemu%2F100:4:::::8::
+  pm_api_url      = "https://192.168.127.134:8006/#v1:0:=qemu%2F100:4:::::8::"
   pm_user         = root
   pm_password     = rootroot
   pm_tls_insecure = true
