e0092a3b0bec9a603eb6ee1f4bb4f85375ae4d0f
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-21 15:39:32 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,27 @@
-variable "pm_api_url" {
-  description = ""
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
+  pm_tls_insecure = true
 }
 
-variable "pm_user" {
-  description = ""
-}
-
-variable "pm_password" {
-  description = ""
-}
-
-variable "target_node" {
-  description = "Name of the Proxmox node"
-}
-
-variable "target_storage" {
-  description = "Name of the Proxmox storage"
-}
-
-variable "bridge" {
-  description = "Name of the bridge network"
-}
-
-variable "disk_size" {
-  description = "Size of the disk for the VM"
-}
-
-variable "os_type" {
-  description = "Type of the operating system"
+resource "proxmox_vm_qemu" "ubuntu_vm" {
+  name            = "ubuntu-vm"
+  clone           = "template"
+  target_node     = var.target_node
+  target_storage  = var.target_storage
+  full_clone      = true
+  
+  network {
+    model   = "virtio"
+    bridge  = var.bridge
+  }
+  
+  disk {
+    id       = 0
+    storage  = var.target_storage
+    size     = var.disk_size
+  }
+  
+  os_type = var.os_type
 }
