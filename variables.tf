f099eef8fb84c47918260a45e11bc303cb839df4
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-21 15:52:22 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,43 @@
-provider "proxmox" {
-  pm_api_url      = var.pm_api_url
-  pm_user         = var.pm_user
-  pm_password     = var.pm_password
-  pm_tls_insecure = true
+variable "pm_api_url" {
+  description = "URL de l'API Proxmox"
+  default     = "https://192.168.127.134:8006/api2/json"
 }
 
-resource "proxmox_vm_qemu" "ubuntu_vm" {
-  name            = "ubuntu-vm"
-  clone           = "template"
-  target_node     = var.target_node
-  target_storage  = var.target_storage
-  full_clone      = true
-  
-  network {
-    model   = "virtio"
-    bridge  = var.bridge
-  }
-  
-  disk {
-    id       = 0
-    storage  = var.target_storage
-    size     = var.disk_size
-  }
+variable "pm_user" {
+  description = "Nom d'utilisateur Proxmox"
+  default     = "root"
+}
+
+variable "pm_password" {
+  description = "Mot de passe Proxmox"
+  default     = "rootroot"
+}
+
+variable "target_node" {
+  description = "Nom du nœud Proxmox"
+  default     = "node1"  // Remplacez "node1" par le nom de votre nœud Proxmox
+}
+
+variable "target_storage" {
+  description = "Nom du stockage Proxmox"
+  default     = "local"  // Remplacez "local" par le nom de votre stockage Proxmox
+}
+
+variable "bridge" {
+  description = "Nom du réseau bridge"
+  default     = "vmbr0"  // Remplacez "vmbr0" par le nom de votre pont réseau Proxmox
+}
+
+variable "disk_size" {
+  description = "Taille du disque pour la machine virtuelle"
+  default     = "10G"    // Remplacez "10G" par la taille souhaitée pour le disque de la VM
+}
+
+variable "os_type" {
+  description = "Type du système d'exploitation"
+  default     = "ubuntu" // Remplacez "ubuntu" par le type de système d'exploitation de votre choix
+}
+
   
   os_type = var.os_type
 }
