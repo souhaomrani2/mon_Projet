3a6a25f4b3b60820f585b1bc0d7567668e3e1d0f
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-27 21:41:06 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "pm_api_url" {
   default     = "https://192.168.127.134:8006/api2/json"
 }
 
+variable "pm_token" {
+  description = "Token d'authentification Proxmox"
+  type        = string
+}
+
 variable "pm_user" {
   description = "Nom d'utilisateur Proxmox"
   default     = "root"
@@ -43,3 +48,8 @@ variable "clone_template" {
   default = "template"
 }
 
+variable "template" {
+  description = "Nom du modèle de machine virtuelle"
+  type        = string
+  default     = "101"  # Remplacez par l'ID de votre modèle de machine virtuelle sur PVE
+}
