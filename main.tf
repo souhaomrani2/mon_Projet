779f105f7c8bbcd94dd2dd0546c5e5d123fd910b
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-27 16:53:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,7 @@ terraform {
 
 provider "proxmox" {
   pm_api_url      = var.pm_api_url
-  pm_user         = var.pm_user
-  pm_password     = var.pm_password
+  pm_token        = var.pm_token  # Utilisation du jeton d'authentification
   pm_tls_insecure = true
 }
 
