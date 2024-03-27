2335dd7122e7bb954b10b0a9a3111e34156bd33e
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-27 22:08:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
-variable "template" {
-  description = "Nom du modèle de machine virtuelle"
-  type        = string
-  default     = "101"  # Remplacez par l'ID de votre modèle de machine virtuelle sur PVE
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
 }
 
 provider "proxmox" {
   pm_api_url      = var.pm_api_url
-  pm_token        = var.pm_token
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
   pm_tls_insecure = true
 }
 
 resource "proxmox_vm_qemu" "ubuntu_vm" {
   name            = "ubuntu-vm"
-  template        = var.template
+  clone           = var.template
   target_node     = var.target_node
   full_clone      = true
   network {
@@ -23,8 +27,9 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
   disk {
     storage  = var.target_storage
     size     = var.disk_size
-    type     = "scsi"
+    type     = "scsi" # Ajout de l'argument type requis pour le disque
   }
   
   os_type = var.os_type
 }
+
