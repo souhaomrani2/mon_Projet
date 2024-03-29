dfa2d0a658fc9681ebeda7f655039c0107b99a86
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-03-29 19:47:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,34 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_api_token_id     = var.pm_user
+  pm_api_token_secret = var.pm_password
+  pm_tls_insecure = true
+}
+
 resource "proxmox_vm_qemu" "ubuntu_vm" {
   name            = "ubuntu-vm"
   clone           = var.template
   target_node     = var.target_node
   full_clone      = true
-
   network {
     model   = "virtio"
     bridge  = var.bridge
   }
   
   disk {
-    storage = var.target_storage
-    size    = var.disk_size
-    type    = "scsi"
+    storage  = var.target_storage
+    size     = var.disk_size
+    type     = "scsi" # Ajout de l'argument type requis pour le disque
   }
-
-  os_type         = var.os_type
-
-  // Arguments pour l'image ISO
-  agent           = true
-  boot            = "cdn"
-  scsihw          = "virtio-scsi-pci"
-
-  // Démarrage de la machine virtuelle avec l'image ISO
-  cdrom           = var.iso_image
+  
+  os_type = var.os_type
 }
