1ce9126fac9b090d9f8fb6c25f7f7a505f615344
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-01 22:58:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url      = var.pm_api_url
-  pm_api_token_id     = var.pm_user
+  pm_api_url         = var.pm_api_url
+  pm_api_token_id    = var.pm_user
   pm_api_token_secret = var.pm_password
-  pm_tls_insecure = true
+  pm_tls_insecure    = true
 }
 
 resource "proxmox_vm_qemu" "ubuntu_vm" {
@@ -19,16 +19,24 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
   clone           = var.template
   target_node     = var.target_node
   full_clone      = true
+
   network {
     model   = "virtio"
     bridge  = var.bridge
   }
-  
+
   disk {
     storage  = var.target_storage
     size     = var.disk_size
-    type     = "scsi" # Ajout de l'argument type requis pour le disque
+    type     = "scsi"
   }
-  
+
+  cdrom {
+    file    = var.iso
+    media   = "cdrom"
+    hotplug = true
+  }
+
   os_type = var.os_type
 }
+
