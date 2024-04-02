96e926c13c571c102af7af3792456d2106731d80
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-02 18:45:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,22 +8,27 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url         = var.pm_api_url
-  pm_api_token_id    = var.pm_user
+  pm_api_url      = var.pm_api_url
+  pm_api_token_id     = var.pm_user
   pm_api_token_secret = var.pm_password
-  pm_tls_insecure    = true
+  pm_tls_insecure = true
 }
 
 resource "proxmox_vm_qemu" "ubuntu_vm" {
-  name        = "ubuntu-vm"
-  target_node = var.target_node
-
-  # Configuration du disque
-  disks {
-    storage      = var.target_storage
-    size         = var.disk_size
-    storage_type = var.storage_type
+  name            = "ubuntu-vm"
+  clone           = var.template
+  target_node     = var.target_node
+  full_clone      = true
+  network {
+    model   = "virtio"
+    bridge  = var.bridge
   }
-
+  
+  disk {
+    storage  = var.target_storage
+    size     = var.disk_size
+    type     = "scsi" # Ajout de l'argument type requis pour le disque
+  }
+  
   os_type = var.os_type
 }
