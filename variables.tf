0e777e8a6e126a2e5ce8052597235ff1b22271ad
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-03 01:21:07 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,26 +19,6 @@ variable "target_node" {
   default     = "pve"  // Remplacez "pve" par le nom de votre nœud Proxmox
 }
 
-variable "target_storage" {
-  description = "Nom du stockage Proxmox"
-  default     = "local-lvm"  // Remplacez "local" par le nom de votre stockage Proxmox
-}
-
-variable "bridge" {
-  description = "Nom du réseau bridge"
-  default     = "vmbr0"  // Remplacez "vmbr0" par le nom de votre pont réseau Proxmox
-}
-
-variable "disk_size" {
-  description = "Taille du disque pour la machine virtuelle"
-  default     = "20G"    // Remplacez "10G" par la taille souhaitée pour le disque de la VM
-}
-
-variable "os_type" {
-  description = "Type du système d'exploitation"
-  default     = "l26" // Remplacez "ubuntu" par le type de système d'exploitation de votre choix
-}
-
 variable "template" {
   description = "Modèle pour la création de la machine virtuelle"
   type        = string
